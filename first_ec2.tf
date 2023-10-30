94fbaac1ac144cff6bc27c80bbf760a1bca9a620
Kishor8066 <104002821+Kishor8066@users.noreply.github.com>
2023-10-30 13:53:17 +0530

    Create first_ec2.tf
    adding first_ec2.tf file

---
+++ first_ec2.tf
@@ -0,0 +1,17 @@
+provider "aws" { 
+  region     = "us-east-1" 
+} 
+
+resource "aws_instance" "myec2" { 
+    ami = "ami-08a52ddb321b32a8c" 
+    instance_type = "t2.micro" 
+     
+    tags = { 
+      Name = "fileserver01.cisco.com" 
+    } 
+}
+
+
+terraform init   : first command you need to run
+terraform plan    : It will plan as per your tf file.
+terraform apply
